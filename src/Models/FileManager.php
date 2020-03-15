34b4c14d2c0fc7ede0308241a2ca57dfcaf7583f
Aguinaldo Tupy <a.tupy@webteam.pt>
2020-03-15 16:51:51 +0000

    accept laravel 7

--- src/Models/FileManager.php
+++ src/Models/FileManager.php
@@ -4,6 +4,32 @@ namespace Tupy\FileManager\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class FileManager
+ * @package Webteam\FileManager\Models
+ * @mixin \Eloquent
+ * @property int $id
+ * @property string $fileable_type
+ * @property int $fileable_id
+ * @property int $uploaded_by
+ * @property string $type
+ * @property string $full_name
+ * @property string $name
+ * @property string $mime_type
+ * @property string $extension
+ * @property int $size
+ * @property string $path_storage
+ * @property string $disk
+ * @property string $visibility
+ * @property string|null $thumbnail
+ * @property string|null $tags
+ * @property string|null $observation
+ * @property int $order
+ * @property string|null $caption
+ * @property string|null $origin
+ * @property string|null $true_timestamp
+ * @property string|null $expiration_date
+ */
 class FileManager extends Model
 {
     /**
@@ -26,7 +52,7 @@ class FileManager extends Model
      * @var array
      */
  	protected $fillable = [
-        'fileable_id', 'fileable_type', 'type', 'full_name', 'name', 'mime_type', 'extension', 'size', 'path_storage', 'disk', 'visibility', 'thumbnail', 'tags', 'observation', 'caption', 'order', 'origem', 'expiration_date', 'true_timestamp'
+        'fileable_id', 'fileable_type', 'uploaded_by', 'type', 'full_name', 'name', 'mime_type', 'extension', 'size', 'path_storage', 'disk', 'visibility', 'thumbnail', 'tags', 'observation', 'caption', 'order', 'origem', 'expiration_date', 'true_timestamp'
     ];
 
     /**
@@ -35,5 +61,20 @@ class FileManager extends Model
     public function fileable()
     {
         return $this->morphTo();
+    }
+
+    public function deletePhoto()
+    {
+        return \Storage::disk($this->disk)->delete($this->full_name);
+
+    }
+
+    public function getPrivateUrlAttribute()
+    {
+        if ($this->full_name && Storage::disk($this->disk)->exists($this->full_name)) {
+            return \Storage::disk($this->disk)->temporaryUrl($this->full_name, now()->addMinutes(5));
+        }
+
+        return false;
     }
 }
